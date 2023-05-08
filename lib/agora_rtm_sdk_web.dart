799e8d6df112e0c07677807619eab04f8bada0c4
Moneer Octedia <moneeroctedia@Moneers-MacBook-Pro.local>
2023-05-08 16:53:00 +0300

    first commit

---
+++ lib/agora_rtm_sdk_web.dart
@@ -0,0 +1,1 @@
+export 'src/agora_rtm_sdk.dart';
---
+++ lib/src/agora_rtm_sdk.dart
@@ -0,0 +1,10 @@
+@JS('AgoraRTM')
+library agora_rtm_sdk;
+
+import 'package:js/js.dart';
+
+external dynamic createInstance(String appId, dynamic config);
+
+// class RtmClient {
+//   external
+// }
